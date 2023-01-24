39092ac2107488b383906d58a22a53a042b06bcb
Mo'men Sherif <momensherif.2019@gmail.com>
2023-01-24 14:06:14 +0200

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-br from-indigo-500 to-teal-500">
+        Hello, World!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
